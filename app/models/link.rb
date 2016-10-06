8ebea41a8bbd1970c0c2de03c5df3d879f6dde0a
George <georgegorbanev@gmail.com>
2016-10-06 23:18:03 +0300

    +generate_links

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,15 @@
 class Link < ApplicationRecord
+  belongs_to :user
   has_many :transitions
+
+  def generate_link(length)
+    valid_symbols = "abcdefghikjklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    result_link = ""
+    length.times do
+      next_symbol = valid_symbols[rand(62)]
+      result_link = result_link + next_symbol
+    end
+    # check db
+    return result_link
+  end
 end
--- db/migrate/20161005092206_devise_create_users.rb
+++ db/migrate/20161005092206_devise_create_users.rb
@@ -32,6 +32,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
 
 
       t.timestamps null: false
+
     end
 
     add_index :users, :email,                unique: true
