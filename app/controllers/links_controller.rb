56d8cdca05b90c5e7744041a7f5634e3b0987a67
George <georgegorbanev@gmail.com>
2016-10-22 22:00:19 +0300

    +create sutom links form, private things

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,11 +1,11 @@
 class LinksController < ApplicationController
 
-  DEFAULT_TABLE_SIZE = 10 # позже добавить возможность менять
-
   def new
-    if params[:type] = "random_public"
+    if params[:type] == "random_public"
       new_link = Link.generate params[:input_link]
       render plain: params[:input_link] + " => " + root_url + new_link + "<br/>"
+    elsif params[:type] == "custom"
+      Link.custom_create params[:user].to_i, params[:child_link], params[:parent_link]
     end
   end
 
@@ -23,6 +23,8 @@ class LinksController < ApplicationController
   end
 
   def index
+    params[:user_id] = current_user.id if current_user != nil
+    redirect_to new_user_session_path if params[:type] == "private" && !user_signed_in?
     params[:commit] == "Search" ?  @links_table = Link.search_table(params) :
                                    @links_table = Link.last_table(params)
   end
@@ -31,7 +33,7 @@ class LinksController < ApplicationController
       @main_path = root_url + "links/" + params[:link_id]
       @table_transactions = Transition.where(:link_id => params[:link_id]).reverse_order
       params[:page] == nil ? @page_number = 1 : @page_number = params[:page].to_i
-      redirect_to root_url + "links/public" if @page_number < 1
+      redirect_to root_url + "links/" if @page_number < 1
   end
 
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -6,6 +6,10 @@ class Link < ApplicationRecord
   belongs_to :user
   has_many :transitions
 
+  def self.custom_create user_id, child, parent
+    Link.create :id => child, :parent_link => parent, :user_id => user_id if Link.find_by(:id => child) == nil
+  end
+
   def self.generate parent_link, user_id = 1, length = COMMON_LENGTH
     generated_link = ""
     loop do
@@ -30,16 +34,17 @@ class Link < ApplicationRecord
     if params[:type] == "private"
       links_table = Link.where(:user_id => params[:user_id]).reverse_order
     elsif params[:type] == "public"
-      links_table = Link.where(:user_id => 1).reverse_order
+      links_table = Link.where(:user_id => 1).order("created_at DESC")
     end
     return links_table
   end
 
   def self.search_table params
+    params[:user_id] && params[:type] == "private" ? user_id = params[:user_id] : user_id = 1
     search_query = " LIKE ? AND user_id  LIKE ?"
     params[:search_column] == "child" ? search_query = "id" + search_query :
                                         search_query = "parent_link" + search_query
-    links_table = Link.where(search_query, "%" + params[:input_link] +"%", 1).reverse_order
+    links_table = Link.where(search_query, "%" + params[:input_link] +"%", user_id).order("created_at DESC")
     return links_table
   end
 
