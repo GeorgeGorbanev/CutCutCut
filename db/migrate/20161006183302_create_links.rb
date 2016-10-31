29d439014f7fc52f98cc4109ed67c80de9e9ec90
George <georgegorbanev@gmail.com>
2016-10-31 15:31:16 +0300

    heroku

--- db/migrate/20161006183302_create_links.rb
+++ db/migrate/20161006183302_create_links.rb
@@ -1,8 +1,7 @@
 class CreateLinks < ActiveRecord::Migration[5.0]
   def change
     create_table :links, :id => false do |t|
-      t.belongs_to :user, index: true
-      t.has_many :transitions
+      t.belongs_to :user, index: true, :foreign_key => :id
       t.string :id, null: false, unique: true
       t.string :parent_link,              null: false
       t.string :name, null: true, default: ""
